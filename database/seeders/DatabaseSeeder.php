b844e969e6d8fe91a7870c7e06812a02474190e2
BabaianuSavelie <babaianu.savelie1@gmail.com>
2023-11-23 14:25:28 +0200

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Brand::factory()
+            ->count(5)
+            ->create();
+
+        Category::factory()
+            ->count(5)
+            ->create();
     }
 }
